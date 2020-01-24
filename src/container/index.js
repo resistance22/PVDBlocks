8acd1d9c625a89be31a978ef65c1715266c16451
amin foroutan <aminforoutan7@gmail.com>
2020-01-24 13:38:58 +0330

    adding the backgroundImage attribute for bootsrap container

--- src/container/index.js
+++ src/container/index.js
@@ -1,7 +1,7 @@
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
-const { InnerBlocks } = wp.editor;
-const { Button } = wp.components;
+const { InnerBlocks,ColorPalette,InspectorControls  } = wp.editor;
+const { PanelBody } = wp.components;
 // Import SVG as React component using @svgr/webpack.
 // https://www.npmjs.com/package/@svgr/webpack
 import { ReactComponent as Logo } from "../bv-logo.svg";
@@ -10,22 +10,54 @@ import { ReactComponent as Logo } from "../bv-logo.svg";
 registerBlockType("pvd/container", {
    title: __("Bootstrap Container", "podkit"),
    icon: { src: Logo },
-   category: "pvd",
+	category: "pvd",
+	attributes:{
+		backgroundColor:{
+			type:'text',
+			default:'transparent'
+		}
+	},
 
  
    // https://wordpress.org/gutenberg/handbook/designers-developers/developers/block-api/block-edit-save/
-   edit( { className } ) {
-		return (
-			<div className={ className + " container bootstrap-container"}>
+   edit: props => {
+		const {className,attributes,setAttributes} = props;
+		const styles = {
+			backgroundColor:attributes.backgroundColor
+		}
+		const onBackgroundChange = (backgroundColor)=>{
+			console.log(backgroundColor);
+			setAttributes({backgroundColor})
+		}
+		return [
+			<InspectorControls>
+				<PanelBody title={__('color settings','pvd')}>
+					<div className="components-base-control">
+						<div className="components-base-control__field">
+							<label className="components-base-control__label">
+								{__("Background color", "podkit")}
+							</label>
+							<ColorPalette 
+								 value={attributes.backgroundColor}
+								 onChange={onBackgroundChange}
+							/>
+						</div>
+					</div>
+				</PanelBody>
+			</InspectorControls>
+			,<div style={styles} className={ className + " container bootstrap-container"}>
 				<InnerBlocks />
 			</div>
-		);
+		]
 	},
 
 	save(props) {
-      const {className} = props;
+		const {attributes} = props;
+		const styles = {
+			backgroundColor:attributes.backgroundColor
+		}
 		return (
-			<div className="container bootstrap-container">
+			<div style={styles} className="container bootstrap-container">
 				<InnerBlocks.Content />
 			</div>
 		);
